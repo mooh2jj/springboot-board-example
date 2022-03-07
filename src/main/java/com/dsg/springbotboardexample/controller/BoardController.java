b1b177263e9c35d2330b4e84c1fc483f5c77a007
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-08 02:38:17 +0900

    board/write.html layout 구현

--- src/main/java/com/dsg/springbotboardexample/controller/BoardController.java
+++ src/main/java/com/dsg/springbotboardexample/controller/BoardController.java
@@ -2,14 +2,21 @@ package com.dsg.springbotboardexample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello!";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        return "board/write";
     }
 }
