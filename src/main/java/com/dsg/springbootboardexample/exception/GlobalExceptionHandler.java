86b7801d5d661e9ec1bc36188479cbcd6611a9bd
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-20 14:43:00 +0900

    Exception GlobalException 처리 GlobalExceptionHandler에 추가

--- src/main/java/com/dsg/springbootboardexample/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dsg/springbootboardexample/exception/GlobalExceptionHandler.java
@@ -27,7 +27,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(BlogAPIException.class)
-    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(
             BlogAPIException exception,
             WebRequest webRequest){
 
@@ -40,4 +40,16 @@ public class GlobalExceptionHandler {
     }
 
     // global exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(
+            Exception exception,
+            WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
