14f753e2b953bfb7b34872279910220bc9863ee7
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-20 15:46:57 +0900

    Validation 추가 CommentDto에 적용

--- src/main/java/com/dsg/springbootboardexample/controller/api/CommentController.java
+++ src/main/java/com/dsg/springbootboardexample/controller/api/CommentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +20,7 @@ public class CommentController {
     @PostMapping("/boards/{boardId}/comments")
     public ResponseEntity<CommentDto> createComment(
             @PathVariable Long boardId,
-            @RequestBody CommentDto commentDto) {
+            @Valid @RequestBody CommentDto commentDto) {
 
         return new ResponseEntity<>(commentService.createComment(boardId, commentDto), HttpStatus.CREATED);
     }
@@ -41,7 +42,7 @@ public class CommentController {
     public ResponseEntity<CommentDto> updateComment(
             @PathVariable Long boardId,
             @PathVariable(value = "id") Long commentId,
-            @RequestBody CommentDto commentDto
+            @Valid @RequestBody CommentDto commentDto
     ) {
         return ResponseEntity.ok(commentService.updateComment(boardId, commentId, commentDto));
     }
--- src/main/java/com/dsg/springbootboardexample/dto/CommentDto.java
+++ src/main/java/com/dsg/springbootboardexample/dto/CommentDto.java
@@ -2,6 +2,9 @@ package com.dsg.springbootboardexample.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @Builder
@@ -10,7 +13,13 @@ import lombok.*;
 public class CommentDto {
 
     private Long id;
+    @NotEmpty
+    @Size(min = 2, message = "name은 2글자 이상이어야 합니다.")
     private String name;
+    @NotEmpty
+    @Size(min = 10, message = "email은 10글자 이상이어야 합니다.")
     private String email;
+    @NotEmpty
+    @Size(min = 10, message = "body는 10글자 이상이어야 합니다.")
     private String body;
 }
