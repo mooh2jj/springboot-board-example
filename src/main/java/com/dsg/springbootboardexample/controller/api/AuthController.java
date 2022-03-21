65f4dc0b16fc57988c5cc3440912d8a83d5e489e
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-22 00:09:32 +0900

    LoginDto 생성 -> signin 성공시키기 구현

---
+++ src/main/java/com/dsg/springbootboardexample/controller/api/AuthController.java
@@ -0,0 +1,33 @@
+package com.dsg.springbootboardexample.controller.api;
+
+import com.dsg.springbootboardexample.dto.LoginDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsernameOrEmail(),
+                        loginDto.getPassword()
+                ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed in successfully!", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/dsg/springbootboardexample/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.dsg.springbootboardexample.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String usernameOrEmail;
+    private String password;
+}
