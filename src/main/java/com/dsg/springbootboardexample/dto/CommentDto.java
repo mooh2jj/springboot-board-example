ab868e20bfed30496cdec2e2ed5a5cca4d7aa455
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-23 15:16:49 +0900

    swagger @ApiModel, @ApiModelProperty 설정

--- src/main/java/com/dsg/springbootboardexample/dto/CommentDto.java
+++ src/main/java/com/dsg/springbootboardexample/dto/CommentDto.java
@@ -1,10 +1,13 @@
 package com.dsg.springbootboardexample.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@ApiModel(description = "board  Comment information")
 @Getter
 @Setter
 @Builder
@@ -12,13 +15,20 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class CommentDto {
 
+    @ApiModelProperty(value = "board comment id")
     private Long id;
+
+    @ApiModelProperty(value = "board comment name")
     @NotEmpty
     @Size(min = 2, message = "name은 2글자 이상이어야 합니다.")
     private String name;
+
+    @ApiModelProperty(value = "board comment email")
     @NotEmpty
     @Size(min = 10, message = "email은 10글자 이상이어야 합니다.")
     private String email;
+
+    @ApiModelProperty(value = "board comment body")
     @NotEmpty
     @Size(min = 10, message = "body는 10글자 이상이어야 합니다.")
     private String body;
